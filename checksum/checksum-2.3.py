68d653fe1190c171ecf60f097877726c81207c8c
John Deadman <xwsomoy@tutanota.com>
2022-12-28 04:13:46 +0600

    checksum sofware v2.3 - customization

--- checksum/checksum-2.3.py
+++ checksum/checksum-2.3.py
@@ -9,6 +9,7 @@ window.get_themes()  # Get the list of available themes
 window.set_theme("plastik")  # Set the theme
 window.title("Checksum Calculator")
 window.configure(bg='black')  # Set the background color to black
+window.geometry("600x300")  # Set the window size to 600x600
 
 # Create a function to compute the checksums
 def compute_checksums():

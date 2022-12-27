aed8e89ba285a4655aa2f75f7e779212e201ec50
John Deadman <xwsomoy@tutanota.com>
2022-12-28 05:16:48 +0600

    py2exe - cx-Freeze

--- py2exe/Setup.py
+++ py2exe/Setup.py
@@ -1,9 +1,12 @@
 import cx_Freeze
+print("This Program uses 'cx_Freeze' libary\n But No additional Dependence Option in not included")
+foo = input('Py Script Name: ')
+var = input('Exe File Name: ')
 
-executables = [cx_Freeze.Executable("script.py")]
+executables = [cx_Freeze.Executable(foo)]
 
 cx_Freeze.setup(
-    name="Executable Name",
-    options={"build_exe": {"packages": ["os"], "include_files": ["image.png"]}},
+    name=var,
+    options={"build_exe": {"packages": ["os"],}},
     executables=executables
 )
